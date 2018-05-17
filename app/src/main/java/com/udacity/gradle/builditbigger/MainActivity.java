5797d44d77cf14ea9faa28d63928605ecc23b964
aleperf <aleperf@users.noreply.github.com>
2018-05-17 15:48:50 +0200

    refactor: externalize AsyncTask

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -18,13 +18,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.aleperf.jokedisplay.JokeDisplayActivity;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
-import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
-import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity implements MainActivityFragment.JokeLauncher {
@@ -92,42 +86,6 @@ public class MainActivity extends AppCompatActivity implements MainActivityFragm
     @Override
     public void tellJoke() {
         new JokeRetrieverAsyncTask().execute( joke);
-    }
-   static class JokeRetrieverAsyncTask extends AsyncTask<MutableLiveData<String>, Void, String> {
-        private MyApi myApiService = null;
-        private MutableLiveData<String> jokeToDisplay;
-
-        @Override
-        protected String doInBackground(MutableLiveData<String>... params) {
-            if(myApiService == null) {  // Only do this once
-                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        // options for running against local devappserver
-                        // - 10.0.2.2 is localhost's IP address in Android emulator
-                        // - turn off compression when running against local devappserver
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });
-                // end options for devappserver
-
-                myApiService = builder.build();
-            }
-        jokeToDisplay = params[0];
-            try {
-                return myApiService.getJoke().execute().getData();
-            } catch (IOException e) {
-                return e.getMessage();
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            jokeToDisplay.setValue(result);
-        }
     }
 
 }
